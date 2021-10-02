bb94a5421c8d0359afa79df056e6bae3201a9c40
Kaarthika2982 <91535010+Kaarthika2982@users.noreply.github.com>
2021-10-02 13:07:39 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,12 @@
+<TextView>
+        android:id="@+id/listview"
+                android:layout_width="410dp"
+                android:layout_height="45dp"
+                android:background="#FF9900"
+                android:backgroundTint="@null"
+                android:text="@string/new_request"
+                android:textAppearance="@style/TextAppearance.AppCompat.Large"
+                android:textColor="#37474F"
+                android:textStyle="bold|italic"
+                android:visibility="visible"
+                app:backgroundTint="@null" />
